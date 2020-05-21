931818e6c7a54dda62a9eca59a02764ebb8f0188
JosewLuis <joseluiscumbrera@gmail.com>
2020-05-21 13:29:01 +0200

    En rama

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+	std::cout << "Crearemos bucles:" << std::endl;
+
+	for(int i=0;i<10;i++){
+		std::cout << i << " ";
+	}
+
+	return 0;
+}
--- Prueba.cpp
+++ Prueba.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 
 int main(){
-	std::cout << "Hola mundo! << std::endl;	
-	std::cout << "Hola mundo! << std::endl;
+	std::cout << "Hola mundo!" << std::endl;	
+	std::cout << "Como primer paso sumaremos" << std::endl;
+
+	int i=0;
+	i=i+1;
 
 	return 0;
 }
